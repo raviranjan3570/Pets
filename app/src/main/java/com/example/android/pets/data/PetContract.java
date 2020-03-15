f3937d5cc92e96f8e545cb22e3a0c699f29bd193
Ravi Ranjan <raviranjan3570@gmail.com>
2020-03-15 12:47:02 +0530

    Initial commit with contract for pets

---
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -0,0 +1,24 @@
+package com.example.android.pets.data;
+
+import android.provider.BaseColumns;
+
+public final class PetContract {
+
+    public static abstract class PetsEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "pets";
+        public static final String COLUMN_ID = BaseColumns._ID;
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_BREED = "breed";
+        public static final String COLUMN_GENDER = "gender";
+        public static final String COLUMN_WEIGHT = "weight";
+
+        /**
+         * Possible values for the gender of the pets.
+         */
+        public static final int GENDER_OTHER= 0;
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
+    }
+
+}
