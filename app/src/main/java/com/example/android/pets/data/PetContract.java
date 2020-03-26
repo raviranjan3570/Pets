128cdba4d567af10d5de7d794ccd3279761d0b13
Ravi Ranjan <raviranjan3570@gmail.com>
2020-03-26 17:55:50 +0530

    PetProvider and URIs.
    Signed-off-by: Ravi Ranjan <raviranjan3570@gmail.com>

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,8 +1,31 @@
 package com.example.android.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public final class PetContract {
+
+    /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.example.android.pets/pets/ is a valid path for
+     * looking at pet data. content://com.example.android.pets/staff/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "staff".
+     */
+    public static final String PATH_PETS = "pets";
 
     public static abstract class PetsEntry implements BaseColumns {
 
@@ -13,15 +36,21 @@ public final class PetContract {
         public static final String COLUMN_PET_BREED = "breed";
         public static final String COLUMN_PET_GENDER = "gender";
         public static final String COLUMN_PET_WEIGHT = "weight";
+
+        /**
+         * The content URI to access the pet data in the provider
+         */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
+
         /**
          * Possible values for the gender of the pets.
          */
         public static final int GENDER_UNKNOWN = 0;
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
 
         public PetsEntry() {
         }
-        public static final int GENDER_MALE = 1;
-        public static final int GENDER_FEMALE = 2;
     }
 
 }
